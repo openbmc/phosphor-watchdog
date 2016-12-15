c4551180789e16d7185fd6e7fb65c2af2cc20ece
Yi Li <adamliyi@msn.com>
2016-12-15 15:31:41 +0800

    Add an openbmc watchdog implementation
    This implements a generic watchdog using interface
    /xyz/openbmc_project/State/Watchdog.interface.yaml.
    
    Resolves: openbmc/openbmc#767
    
    Change-Id: I0824cb55faed638d18d9f90ea45db8f490df3f3c
    Signed-off-by: Yi Li <adamliyi@msn.com>

---
+++ test/utest.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+#include "watchdog.hpp"
+
+using namespace phosphor::state::watchdog;
+
+constexpr auto testObjpath = "/watchdog/test";
+constexpr auto testService = "watchdog.test";
+
+class WatchdogTest : public testing::Test
+{
+    public:
+        std::string objpath;
+        std::string service;
+        sdbusplus::bus::bus bus;
+        sd_event* timerEvent;
+        Watchdog watchdog;
+
+        WatchdogTest()
+            : objpath(testObjpath),
+              service(testService),
+              bus(sdbusplus::bus::new_default()),
+              timerEvent(nullprt)
+        {
+        }
+
+        ~WatchdogTest()
+        {
+        }
+
+        virtual void SetUp()
+        {
+            sdbusplus::server::manager::manager objManager(bus,
+                                                    objpath.c_str());
+            bus.request_name(service.c_str());
+            // acquire a referece to the default event loop
+            sd_event_default(&timerEvent);
+            // attach bus to this event loop
+            bus.attach_event(timerEvent, SD_EVENT_PRIORITY_NORMAL);
+            watchdog = Watchdog(bus, objpath.c_str());
+        }
+
+        virtual void TearDown()
+        {
+            bus.detach_event();
+            sd_event_unref(timerEvent);
+        }
+};
+
+/** @brief Get watchdog default interval */
+TEST_F(WatchdogTest, getInterval)
+{
+    // default interval is 30000 msec
+    EXPECT_EQ(30000, watchdog.interval());
+}
+
+/** @brief Set watchdog interval */
+TEST_F(WatchdogTest, setInterval)
+{
+    watchdog.interval(10000);
+    EXPECT_EQ(10000, watchdog.interval());
+}
+
+/** @brief Get remaining time */
+TEST_F(WatchdogTest, getRemainTime)
+{
+    // if not enabled, expect 0
+    EXPECT_EQ(0, watchdog.timeRemaining());
+    watchdog.enabled(true);
+    // if enabled, expect a positive value
+    EXPECT_GT(watchdog.timeRemaining(), 0);
+    watchdog.enabled(false);
+    EXPECT_EQ(0, watchdog.timeRemaining());
+}
+
+/** @brief Set remaining time */
+TEST_F(WatchdogTest, setRemainTime)
+{
+    watchdog.timeRemaining(10000);
+    // if not enabled, set timeRemaining has not effect
+    EXPECT_EQ(0, watchdog.timeRemaining());
+    watchdog.enabled(true);
+    watchdog.timeRemaining(10000);
+    // expect the timeRemaining between 0 and 10000 msec
+    EXPECT_GT(watchdog.timeRemaining(), 0);
+    EXPECT_LE(watchdog.timeRemaining(), 10000);
+}
+
+// Todo, and test to check Timeout signal
---
+++ watchdog.cpp
@@ -0,0 +1,207 @@
+#include <chrono>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <log.hpp>
+#include <systemd/sd-bus.h>
+#include <sdbusplus/vtable.hpp>
+#include "watchdog.hpp"
+
+namespace phosphor
+{
+namespace state
+{
+namespace watchdog
+{
+using namespace std::chrono;
+using namespace phosphor::logging;
+
+/* Return current time in usec, using CLOCK_MONOTONIC.
+ * Watchdog uses CLOCK_MONOTONIC for sd-event timer. */
+uint64_t Watchdog::now() const
+{
+    struct timespec ts{};
+    auto r = clock_gettime(CLOCK_MONOTONIC, &ts);
+    if (r == -1)
+    {
+        log<level::ERR>("watchdog: clock_gettime() error",
+                entry("ERROR=%s", strerror(errno)));
+        return 0;
+    }
+
+    // convert to microsecond
+    auto currTime = duration_cast<microseconds>
+                    (seconds(ts.tv_sec) + nanoseconds(ts.tv_nsec));
+
+    return currTime.count();
+}
+
+bool Watchdog::enabled(bool value)
+{
+    int r = 0;
+
+    if (WatchdogInherits::enabled() != value)
+    {
+        if (value)
+        {
+            // start the one-shot timer
+            r = setTimer(interval());
+            if (r < 0)
+            {
+                return false;
+            }
+
+            r = setTimerEnabled(true);
+            if (r < 0)
+            {
+                return false;
+            }
+
+            log<level::INFO>("watchdog: enabled and started",
+                             entry("INTERVAL=%llu", interval()));
+        }
+        else
+        {
+            r = setTimerEnabled(false);
+            if (r < 0)
+            {
+                return true;
+            }
+
+            log<level::INFO>("watchdog: disabled");
+        }
+    }
+    return WatchdogInherits::enabled(value);
+}
+
+/* get the remaining time before timer expires.
+ * if the timer is disabled, returns 0 */
+uint64_t Watchdog::timeRemaining() const
+{
+    uint64_t timeRemain = 0;
+    int enabled = 0;
+
+    if (WatchdogInherits::enabled())
+    {
+        // timer may already expire and disabled
+        auto r = sd_event_source_get_enabled(timerEventSource, &enabled);
+        if (r < 0)
+        {
+            log<level::ERR>(
+                "watchdog timeRemaining: sd_event_source_get_enabled() error",
+                entry("ERROR=%s", strerror(-r)));
+            return 0;
+        }
+
+        if (enabled != SD_EVENT_OFF)
+        {
+            // the one-shot timer does not expire yet
+            uint64_t next = 0;
+
+            /* get the configured expire time, in usec */
+            r = sd_event_source_get_time(timerEventSource, &next);
+            if (r < 0)
+            {
+                log<level::ERR>(
+                    "watchdog timeRemaining: sd_event_source_get_time() error",
+                    entry("ERROR=%s", strerror(-r)));
+                return 0;
+            }
+            // convert to msec
+            auto nextMsec = duration_cast<milliseconds>(microseconds(next));
+            auto nowMsec = duration_cast<milliseconds>(microseconds(now()));
+            timeRemain = (nextMsec > nowMsec) ?
+                            (nextMsec - nowMsec).count() : 0;
+        }
+    }
+
+    return timeRemain;
+}
+
+/* reset the timer to @value if the timer is enabled */
+uint64_t Watchdog::timeRemaining(uint64_t value)
+{
+    if (WatchdogInherits::enabled())
+    {
+        auto r = setTimerEnabled(false);
+        if (r < 0)
+        {
+            return 0;
+        }
+
+        r = setTimer(value);
+        if (r < 0)
+        {
+            return 0;
+        }
+
+        r = setTimerEnabled(true);
+        if (r < 0)
+        {
+            return 0;
+        }
+
+        log<level::INFO>("watchdog: reset timer",
+                         entry("VALUE=%llu", value));
+
+        // Invoke property_changed("timeRemaining")
+        return WatchdogInherits::timeRemaining(value);
+    }
+
+    return 0;
+}
+
+/* the static watchdog timout handler */
+int Watchdog::timeoutHandler(sd_event_source* es, uint64_t usec, void* userData)
+{
+    auto wdg = static_cast<Watchdog*>(userData);
+
+    // invoke dbus signal
+    log<level::ALERT>("watchdog: time out");
+    wdg->timeout();
+
+    return 0;
+}
+
+// Enable or disable timer: @enable: true to enable, false to disable.
+int Watchdog::setTimerEnabled(bool enable)
+{
+
+    // One-shot timer: the event source will be enabled but automatically
+    // reset to SD_EVENT_OFF after the event source was dispatched once
+    auto state = enable ? SD_EVENT_ONESHOT : SD_EVENT_OFF;
+
+    auto r = sd_event_source_set_enabled(timerEventSource, state);
+    if (r < 0)
+    {
+        log<level::ERR>
+            ("watchdog setTimerEnabled: sd_event_source_set_enabled() error",
+            entry("ERROR=%s", strerror(-r)));
+    }
+
+    return r;
+}
+
+// @value in msec
+int Watchdog::setTimer(uint64_t value)
+{
+    // convert to usec
+    auto nextUsec = duration_cast<microseconds>(milliseconds(value));
+    auto next = now() + nextUsec.count();
+
+    /* set next expire time */
+    auto r = sd_event_source_set_time(timerEventSource, next);
+    if (r < 0)
+    {
+        log<level::ERR>
+            ("watchdog setTimer: sd_event_source_set_set_time() error",
+            entry("ERROR=%s", strerror(-r)));
+    }
+
+    return r;
+}
+
+} // namespace watchdog
+} // namespace state
+} // namepsace phosphor
