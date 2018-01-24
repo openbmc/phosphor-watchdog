e335ed9dd1503c2a736996c1de9d6700742a7d8f
William A. Kennington III <wak@google.com>
2018-01-23 22:13:44 -0800

    argument: Implement test cases
    The argument parser had a few subtle bugs and could use some tests.
    
    Change-Id: I0adbc507a822a6887fbf02b888b1f678b3cc7b35
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ test/argument_test.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+#include <gtest/gtest.h>
+
+namespace phosphor
+{
+namespace watchdog
+{
+
+class ArgumentTest : public testing::Test
+{
+    protected:
+        void SetUp() override;
+
+        std::string arg0;
+};
+
+}  // namespace watchdog
+}  // namespace phosphor
