15c5bd1b60300255d9dd2ffdd77eaf6dfce51722
Patrick Williams <patrick@stwcx.xyz>
2021-09-02 09:51:29 -0500

    exception: switch to public sdbus exception
    SdBusError was intended to be a private error type inside sdbusplus.
    Switch all catch locations to use the general sdbusplus::exception type.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Id531dea23a3084fc0d1e4c5e0504e944bd619b05

--- src/watchdog.cpp
+++ src/watchdog.cpp
@@ -16,7 +16,6 @@ using namespace std::chrono;
 using namespace std::chrono_literals;
 using namespace phosphor::logging;
 
-using sdbusplus::exception::SdBusError;
 using sdbusplus::xyz::openbmc_project::Common::Error::InternalFailure;
 
 // systemd service to kick start a target.
@@ -139,7 +138,7 @@ void Watchdog::timeOutHandler()
             signal.append(convertForMessage(action).c_str());
             signal.signal_send();
         }
-        catch (const SdBusError& e)
+        catch (const sdbusplus::exception::exception& e)
         {
             log<level::ERR>("watchdog: failed to send timeout signal",
                             entry("ERROR=%s", e.what()));
@@ -154,7 +153,7 @@ void Watchdog::timeOutHandler()
 
             bus.call_noreply(method);
         }
-        catch (const SdBusError& e)
+        catch (const sdbusplus::exception::exception& e)
         {
             log<level::ERR>("watchdog: Failed to start unit",
                             entry("TARGET=%s", target->second.c_str()),
