46657750d5989c6dadd13f2001569caccf64f74b
Willy Tu <wltu@google.com>
2021-12-27 09:57:25 -0800

    phosphor-watchdog: Convert watchdog to ASIO to remove sdevent usage
    Running the following three actions with lots of postcodes will cause
    memory to leak and crash the watchdog.
    - Sd event/timer
    - Sd signal matching
    - Ipmi requests
    
    Refactor the code to use boost::asio to remove the burden and handle the
    memory usage better.
    
    Tested:
    
    Before,
    ```
    $ ipmitool mc watchdog get             -
    Watchdog Timer Use:     SMS/OS (0x44)
    Watchdog Timer Is:      Started/Running
    Watchdog Timer Logging: On
    Watchdog Timer Action:  Hard Reset (0x01)
    Pre-timeout interrupt:  None
    Pre-timeout interval:   0 seconds
    Timer Expiration Flags: None (0x00)
    Initial Countdown:      60.0 sec
    Present Countdown:      56.2 sec
    ```
    
    After,
    ```
    ipmitool mc watchdog get
    Watchdog Timer Use:     SMS/OS (0x04)
    Watchdog Timer Is:      Stopped
    Watchdog Timer Logging: On
    Watchdog Timer Action:  Hard Reset (0x01)
    Pre-timeout interrupt:  None
    Pre-timeout interval:   0 seconds
    Timer Expiration Flags: None (0x00)
    Initial Countdown:      30.0 sec
    Present Countdown:      30.0 sec
    ```
    
    Change-Id: Iff6c260d0404e42b6a560defb5e835695cbfd675
    Signed-off-by: Willy Tu <wltu@google.com>

--- src/watchdog.hpp
+++ src/watchdog.hpp
@@ -1,14 +1,17 @@
 #pragma once
 
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <chrono>
 #include <functional>
+#include <memory>
 #include <optional>
-#include <sdbusplus/bus.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/asio/sd_event.hpp>
 #include <sdbusplus/server/object.hpp>
-#include <sdeventplus/event.hpp>
-#include <sdeventplus/utility/timer.hpp>
 #include <string_view>
 #include <unordered_map>
-#include <utility>
 #include <xyz/openbmc_project/State/Watchdog/server.hpp>
 
 namespace phosphor
@@ -16,24 +19,115 @@ namespace phosphor
 namespace watchdog
 {
 
+class Timer
+{
+  public:
+    Timer() = delete;
+    ~Timer() = default;
+    Timer(const Timer&) = delete;
+    Timer& operator=(const Timer&) = delete;
+    Timer(Timer&&) = delete;
+    Timer& operator=(Timer&&) = delete;
+
+    Timer(boost::asio::io_service& io, std::function<void()> handler) :
+        asyncTimer(io),
+        timeoutHandler([this, handler](const boost::system::error_code& e) {
+            if (e == boost::asio::error::operation_aborted)
+            {
+                // Timer was  cancelled, skip any action.
+                return;
+            }
+
+            enabled = false;
+            handler();
+        }),
+        enabled(false){};
+
+    void start(uint64_t interval)
+    {
+        // Start Watchdog Timer
+        asyncTimer.expires_after(std::chrono::milliseconds(interval));
+        asyncTimer.async_wait(timeoutHandler);
+        enabled = true;
+    }
+
+    void setEnabled(bool enable)
+    {
+        if (!enable)
+        {
+            asyncTimer.cancel();
+        }
+
+        enabled = enable;
+    }
+
+    bool hasExpired() const
+    {
+        return asyncTimer.expiry() < std::chrono::steady_clock::now();
+    }
+
+    bool isEnabled() const
+    {
+        return enabled;
+    }
+
+    void restart(std::chrono::milliseconds interval)
+    {
+        setRemaining(interval);
+    }
+
+    std::chrono::nanoseconds getRemaining() const
+    {
+        if (!enabled || asyncTimer.expiry() < std::chrono::steady_clock::now())
+        {
+            return std::chrono::nanoseconds(0);
+        }
+
+        return asyncTimer.expiry() - std::chrono::steady_clock::now();
+    }
+
+    void setRemaining(std::chrono::milliseconds interval)
+    {
+        asyncTimer.expires_after(interval);
+        asyncTimer.async_wait(timeoutHandler);
+        enabled = true;
+    }
+
+  private:
+    /** @brief Watchdog Timer */
+    boost::asio::steady_timer asyncTimer;
+
+    /** @brief Watchdog Timeout Handler */
+    std::function<void(const boost::system::error_code&)> timeoutHandler;
+
+    /** @brief Watchdog Timer Enabled State */
+    bool enabled;
+};
+
 constexpr auto DEFAULT_MIN_INTERVAL_MS = 0;
-namespace Base = sdbusplus::xyz::openbmc_project::State::server;
-using WatchdogInherits = sdbusplus::server::object::object<Base::Watchdog>;
+using WatchdogBase = sdbusplus::xyz::openbmc_project::State::server::Watchdog;
 
 /** @class Watchdog
  *  @brief OpenBMC watchdog implementation.
  *  @details A concrete implementation for the
  *  xyz.openbmc_project.State.Watchdog DBus API.
  */
-class Watchdog : public WatchdogInherits
+class Watchdog
 {
   public:
     Watchdog() = delete;
-    ~Watchdog() = default;
+    virtual ~Watchdog() = default;
     Watchdog(const Watchdog&) = delete;
     Watchdog& operator=(const Watchdog&) = delete;
     Watchdog(Watchdog&&) = delete;
     Watchdog& operator=(Watchdog&&) = delete;
+
+    /** @brief Type used to hold the name of a Watchdog Action.
+     */
+    using Action = WatchdogBase::Action;
+    /** @brief Type used to hold the name of a Watchdog Timer.
+     */
+    using TimerUse = WatchdogBase::TimerUse;
 
     /** @brief Type used to hold the name of a systemd target.
      */
@@ -55,7 +149,8 @@ class Watchdog : public WatchdogInherits
 
     /** @brief Constructs the Watchdog object
      *
-     *  @param[in] bus             - DBus bus to attach to.
+     *  @param[in] io              - Async IO Service
+     *  @param[in] conn            - Dbus connection for Async IO.
      *  @param[in] objPath         - Object path to attach to.
      *  @param[in] event           - reference to sdeventplus::Event loop
      *  @param[in] actionTargets   - map of systemd targets called on timeout
@@ -63,28 +158,30 @@ class Watchdog : public WatchdogInherits
      *  @param[in] minInterval     - minimum intervale value allowed
      *  @param[in] defaultInterval - default interval to start with
      */
-    Watchdog(sdbusplus::bus::bus& bus, const char* objPath,
-             const sdeventplus::Event& event,
-             ActionTargetMap&& actionTargetMap = {},
+    Watchdog(boost::asio::io_service& io,
+             const std::shared_ptr<sdbusplus::asio::connection>& conn,
+             const char* objPath,
+             Watchdog::ActionTargetMap&& actionTargetMap = {},
              std::optional<Fallback>&& fallback = std::nullopt,
              uint64_t minInterval = DEFAULT_MIN_INTERVAL_MS,
              uint64_t defaultInterval = 0) :
-        WatchdogInherits(bus, objPath),
-        bus(bus), actionTargetMap(std::move(actionTargetMap)),
-        fallback(fallback), minInterval(minInterval),
-        timer(event, std::bind(&Watchdog::timeOutHandler, this)),
+        conn(conn),
+        actionTargetMap(std::move(actionTargetMap)), fallback(fallback),
+        minInterval(minInterval), timer(io, [this]() { timeOutHandler(); }),
         objPath(objPath)
     {
+        // Setup all Watchdog State properties.
+        initService();
+
         // Use default if passed in otherwise just use default that comes
         // with object
         if (defaultInterval)
         {
             interval(defaultInterval);
         }
-        else
-        {
-            interval(interval());
-        }
+
+        timer.start(interval());
+
         // We need to poke the enable mechanism to make sure that the timer
         // enters the fallback state if the fallback is always enabled.
         tryFallbackOrDisable();
@@ -95,13 +192,7 @@ class Watchdog : public WatchdogInherits
      *
      *  @param[in] enableWatchdog - Should the call enable the watchdog
      */
-    void resetTimeRemaining(bool enableWatchdog) override;
-
-    /** @brief Since we are overriding the setter-enabled but not the
-     *         getter-enabled, we need to have this using in order to
-     *         allow passthrough usage of the getter-enabled.
-     */
-    using Base::Watchdog::enabled;
+    void resetTimeRemaining(bool enableWatchdog);
 
     /** @brief Enable or disable watchdog
      *         If a watchdog state is changed from disable to enable,
@@ -112,16 +203,16 @@ class Watchdog : public WatchdogInherits
      *
      *  @param[in] value - 'true' to enable. 'false' to disable
      *
-     *  @return : applied value if success, previous value otherwise
+     *  @return : applied value
      */
-    bool enabled(bool value) override;
+    bool enabled(bool value);
 
     /** @brief Gets the remaining time before watchdog expires.
      *
      *  @return 0 if watchdog is expired.
      *          Remaining time in milliseconds otherwise.
      */
-    uint64_t timeRemaining() const override;
+    uint64_t timeRemaining() const;
 
     /** @brief Reset timer to expire after new timeout in milliseconds.
      *
@@ -129,26 +220,18 @@ class Watchdog : public WatchdogInherits
      *                     the watchdog will expire
      *
      *  @return: updated timeout value if watchdog is enabled.
-     *           0 otherwise.
+     *           std::nullopt otherwise.
      */
-    uint64_t timeRemaining(uint64_t value) override;
-
-    /** @brief Get value of Interval
-     *
-     *
-     *  @return: current interval
-     *
-     */
-    using WatchdogInherits::interval;
+    std::optional<uint64_t> timeRemaining(uint64_t value);
 
     /** @brief Set value of Interval
      *
      *  @param[in] value - interval time to set
      *
-     *  @return: interval that was set
+     *  @return: interval that was set.
      *
      */
-    uint64_t interval(uint64_t value) override;
+    uint64_t interval(uint64_t value);
 
     /** @brief Tells if the referenced timer is expired or not */
     inline auto timerExpired() const
@@ -162,9 +245,35 @@ class Watchdog : public WatchdogInherits
         return timer.isEnabled();
     }
 
+    bool enabled() const
+    {
+        return watchdogEnabled;
+    }
+
+    uint64_t interval() const
+    {
+        return std::max(watchdogInterval, minInterval);
+    }
+
+    Action expireAction(Action action)
+    {
+        watchdogExpireAction = action;
+        return watchdogExpireAction;
+    }
+
+    Action expireAction() const
+    {
+        return watchdogExpireAction;
+    }
+
   private:
-    /** @brief sdbusplus handle */
-    sdbusplus::bus::bus& bus;
+    void initService();
+
+    template <typename PropertyType>
+    bool setProperty(std::string_view name, PropertyType value);
+
+    /** @brief Dbus connection for Async IO */
+    std::shared_ptr<sdbusplus::asio::connection> conn;
 
     /** @brief Map of systemd units to be started when the timer expires */
     ActionTargetMap actionTargetMap;
@@ -176,16 +285,41 @@ class Watchdog : public WatchdogInherits
     uint64_t minInterval;
 
     /** @brief Contained timer object */
-    sdeventplus::utility::Timer<sdeventplus::ClockId::Monotonic> timer;
+    Timer timer;
 
-    /** @brief Optional Callback handler on timer expirartion */
+    /** @brief Optional Callback handler on timer expiration */
     void timeOutHandler();
 
     /** @brief Attempt to enter the fallback watchdog or disables it */
     void tryFallbackOrDisable();
 
     /** @brief Object path of the watchdog */
+
     std::string_view objPath;
+
+    /** @brief Watchdog State Properties */
+    std::shared_ptr<sdbusplus::asio::dbus_interface> watchdog;
+
+    /** @brief Watchdog Reset Interval */
+    uint64_t watchdogInterval = 30000;
+
+    /** @brief Watchdog Time Remaining */
+    uint64_t watchdogTimeRemaining = 0;
+
+    /** @brief Watchdog Initialization State */
+    bool watchdogInitialized = false;
+
+    /** @brief Watchdog Enabled State */
+    bool watchdogEnabled = false;
+
+    /** @brief Watchdog Expire Action */
+    Action watchdogExpireAction = Action::HardReset;
+
+    /** @brief Watchdog Current Timer */
+    TimerUse watchdogCurrentTimerUse = TimerUse::Reserved;
+
+    /** @brief Watchdog Expire Timer */
+    TimerUse watchdogExpiredTimerUse = TimerUse::Reserved;
 };
 
 } // namespace watchdog
